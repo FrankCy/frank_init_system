9fc5a41d1a6a904ae88760c3dd997f9489e31cb6
yang.chang <cy880708@163.com>
2023-01-13 18:36:41 +0800

    init

---
+++ InitApp/java.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#-------------------------------------------------#
+# 脚本主要功能说明: jdk安装
+#-------------------------------------------------#
+function java8_22_1 {
+  echo 'java8_22_1 installing being'
+  echo '下载java 8 22 1'
+  echo '清理java'
+  echo '开始安装'
+  echo '验证'
+  echo 'java8_22_1 installing end'
+}
