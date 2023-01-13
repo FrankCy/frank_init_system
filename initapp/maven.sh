5b56a9cb7d7caa677a386cc8d89dccc805aa8e88
yang.chang <cy880708@163.com>
2023-01-13 18:47:07 +0800

    init

---
+++ initapp/maven.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+#-------------------------------------------------#
+# 脚本主要功能说明:
+#-------------------------------------------------#
+function maven3_6 {
+  echo 'maven 3.6 installing being'
+  echo 'maven 3.6 installing end'
+}
