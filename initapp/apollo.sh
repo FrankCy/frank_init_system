8a7ce2c3f0d8198648f44169ab0a91f5c512d1a9
yang.chang <cy880708@163.com>
2023-01-16 21:41:27 +0800

    apollo; maven;

--- initapp/apollo.sh
+++ initapp/apollo.sh
@@ -1,18 +1,138 @@
 #!/bin/bash
 #-------------------------------------------------#
-# 脚本主要功能说明:
+# 脚本主要功能说明:安装Apollo
+# 准备工作：
+# 1：MySQL数据库，初始化Apollo相关表
+# 2：对应版本的URL，可通过wget下载
 #-------------------------------------------------#
+. ./common/common.sh
 function apollo1.4 {
-  echo 'apollo 1.4 installing being'
-  echo 'apollo 1.4 installing end'
+  echo_info 'apollo 1.4 installing being'
+  CONFIG_URL=http://apollo-config-1.4.tar.gz
+  ADMIN_URL=http://apollo-admin-1.4.tar.gz
+  PORTAL_URL=http://apollo-portal-1.4.0%202.zip
+
+  # portal meta service配置，自行修改
+  METAL_LOCAL=http://localhost:8077
+  META_DEV=http://localhost:8077
+  META_FAT=http://localhost:8077
+  META_UAT=http://localhost:8077
+  META_LPT=http://localhost:8077
+  META_PRO=http://localhost:8077
+
+  # 数据库配置
+  MYSQL_CONFIG_URL=jdbc:mysql://127.0.0.1:3306/apollo_config_db?characterEncoding=utf8
+  MYSQL_PORTAL_URL=jdbc:mysql://127.0.0.1:3306/apollo_portal_db?characterEncoding=utf8
+  MYSQL_USER=root
+  MYSQL_PASSWORD=Admin172.16.0.95
+
+  #如果文件夹不存在，则创建文件夹
+  DATA_SERVER_PATH="/data/server/"
+  if [ ! -d "$DATA_SERVER_PATH" ]; then
+    mkdir $DATA_SERVER_PATH
+  else
+    echo_info '文件夹已存在'
+    echo_info '进入文件夹'
+    cd $DATA_SERVER_PATH
+  fi
+  # -----------------------------------------------Apollo Config----------------------------------------------------------------
+  echo_info '开始下载ApolloConfig'
+  wget -O apollo-configservice.tar.gz $CONFIG_URL
+  echo_info '结束下载ApolloConfig'
+
+  echo_info '开始解压ApolloConfig'
+  mkdir ./apollo-configservice && tar -xzvf apollo-configservice.tar.gz -C ./apollo-configservice --strip-components 1
+  echo_info '结束解压ApolloConfig'
+
+  echo_info '开始修改ApolloConfig config'
+  apolloConfigConfigPath=$DATA_SERVER_PATH'apollo-configservice/config/application-github.properties'
+  sed -i -e "s#^spring.datasource.url=.*#spring.datasource.url=${MYSQL_CONFIG_URL}#g" ${apolloConfigConfigPath}
+  sed -i -e "s#^spring.datasource.username=.*#spring.datasource.username=${MYSQL_USER}#g" ${apolloConfigConfigPath}
+  sed -i -e "s#^spring.datasource.password=.*#spring.datasource.password=${MYSQL_PASSWORD}#g" ${apolloConfigConfigPath}
+  echo_info '结束修改ApolloConfig config'
+
+  echo_info '---------------- ApolloConfig config ----------------'
+  cat ${apolloConfigConfigPath}
+  echo_info '---------------- ApolloConfig config ----------------'
+
+  echo_info '开始启动ApolloConfig'
+  sh ./apollo-configservice/scripts/startup.sh > config_start.log 2>&1
+  echo_info '结束启动ApolloConfig'
+
+  # -----------------------------------------------Apollo Admin----------------------------------------------------------------
+
+  cd $DATA_SERVER_PATH
+  echo_info '开始下载ApolloAdmin'
+  wget -O apollo-adminservice.tar.gz $ADMIN_URL
+  echo_info '结束下载ApolloAdmin'
+
+  echo_info '开始解压ApolloAdmin'
+  mkdir ./apollo-adminservice && tar -xzvf apollo-adminservice.tar.gz -C ./apollo-adminservice --strip-components 1
+  echo_info '结束解压ApolloAdmin'
+
+  echo_info '开始修改ApolloAdmin Config'
+  apolloAdminConfigPath=$DATA_SERVER_PATH'apollo-adminservice/config/application-github.properties'
+  sed -i -e "s#^spring.datasource.url=.*#spring.datasource.url=${MYSQL_CONFIG_URL}#g" ${apolloAdminConfigPath}
+  sed -i -e "s#^spring.datasource.username=.*#spring.datasource.username=${MYSQL_USER}#g" ${apolloAdminConfigPath}
+  sed -i -e "s#^spring.datasource.password=.*#spring.datasource.password=${MYSQL_PASSWORD}#g" ${apolloAdminConfigPath}
+  echo_info '结束修改ApolloAdmin Config'
+
+  echo_info '---------------- ApolloAdmin config ----------------'
+  cat ${apolloAdminConfigPath}
+  echo_info '---------------- ApolloAdmin config ----------------'
+
+  echo_info '开始启动ApolloAdmin'
+  sh ./apollo-adminservice/scripts/startup.sh > admin_start.log 2>&1
+  echo_info '结束启动ApolloAdmin'
+
+  # -----------------------------------------------Apollo Portal----------------------------------------------------------------
+
+  cd $DATA_SERVER_PATH
+  echo_info '开始下载ApolloPortal'
+  wget $PORTAL_URL
+  echo_info '结束下载ApolloPortal'
+
+  echo_info '开始解压ApolloPortal'
+  unzip apollo-portal-1.4.0\ 2.zip && mv `unzip -l apollo-portal-1.4.0\ 2.zip | awk '{if(NR == 4){ print $4}}'` apollo-portal
+  echo_info '结束解压ApolloPortal'
+
+  echo_info '开始修改ApolloPortal Config'
+  apolloPortalConfigPath=$DATA_SERVER_PATH'apollo-portal/config/application-github.properties'
+  sed -i -e "s#^spring.datasource.url=.*#spring.datasource.url=${MYSQL_PORTAL_URL}#g" ${apolloPortalConfigPath}
+  sed -i -e "s#^spring.datasource.username=.*#spring.datasource.username=${MYSQL_USER}#g" ${apolloPortalConfigPath}
+  sed -i -e "s#^spring.datasource.password=.*#spring.datasource.password=${MYSQL_PASSWORD}#g" ${apolloPortalConfigPath}
+  echo_info '结束修改ApolloPortal Config'
+
+  echo_info '---------------- ApolloPortal config ----------------'
+  cat ${apolloPortalConfigPath}
+  echo_info '---------------- ApolloPortal config ----------------'
+
+  echo_info '开始修改ApolloPortal Meta Service'
+  apolloPortalMetaPath=$DATA_SERVER_PATH'apollo-portal/config/apollo-env.properties'
+  sed -i -e "s#^local.meta=.*#local.meta=${METAL_LOCAL}#g" ${apolloPortalMetaPath}
+  sed -i -e "s#^dev.meta=.*#dev.meta=${META_DEV}#g" ${apolloPortalMetaPath}
+  sed -i -e "s#^fat.meta=.*#fat.meta=${META_FAT}#g" ${apolloPortalMetaPath}
+  sed -i -e "s#^uat.meta=.*#uat.meta=${META_UAT}#g" ${apolloPortalMetaPath}
+  sed -i -e "s#^lpt.meta=.*#lpt.meta=${METAL_LOCAL}#g" ${apolloPortalMetaPath}
+  sed -i -e "s#^pro.meta=.*#pro.meta=${META_PRO}#g" ${apolloPortalMetaPath}
+  echo_info '结束修改ApolloPortal Meta Service'
+
+  echo_info '---------------- ApolloPortal Meta Service ----------------'
+  cat ${apolloPortalMetaPath}
+  echo_info '---------------- ApolloPortal Meta Service ----------------'
+
+  echo_info '开始启动ApolloPortal'
+  sh ./apollo-portal/scripts/startup.sh > portal_start.log 2>&1
+  echo_info '结束启动ApolloPortal'
+  echo_info 'apollo 1.4 installing end'
 }
 
 function apollo1.6 {
-  echo 'apollo 1.6 installing being'
-  echo 'apollo 1.6 installing end'
+  echo_info 'apollo 1.6 installing being'
+  echo_info 'apollo 1.6 installing end'
 }
 
 function apollo2.0 {
-  echo 'apollo 2.0 installing being'
-  echo 'apollo 2.0 installing end'
+  echo_info 'apollo 2.0 installing being'
+  echo_info 'apollo 2.0 installing end'
 }
--- initapp/maven.sh
+++ initapp/maven.sh
@@ -1,9 +1,31 @@
 #!/bin/bash
 #-------------------------------------------------#
-# 脚本主要功能说明:
+# 脚本主要功能说明:安装maven
+# 准备
+# 1：需要JDK
 #-------------------------------------------------#
 . ./common/common.sh
+# 下载3.6地址
+MAVEN_3_6_URL=https://github.com/apache/maven/archive/refs/tags/maven-3.6.3.tar.gz
+# 安装目录
+INSTALL_PATH=/data/server
 function maven3_6 {
   echo_info 'maven 3.6 installing being'
+  # 下载maven到/tmp下
+  wget $MAVEN_3_6_URL -P /tmp;
+  # 解压到指定目录（/data/server）
+  tar -zxf /tmp/maven-3.6.3.tar.gz -C $INSTALL_PATH;
+  # 设置环境变量
+  nano /etc/profile.d/maven.sh;
+  # 写入配置
+  echo 'export JAVA_HOME=/usr/lib/jvm/default-java
+        export M2_HOME=/opt/maven
+        export MAVEN_HOME=/opt/maven
+        export PATH=${M2_HOME}/bin:${PATH}' >> /etc/profile.d/maven.sh;
+  chmod +x /etc/profile.d/maven.sh;
+  # 生效环境变量
+  source /etc/profile.d/maven.sh;
+  # 检查mvn安装情况
+  mvn -version;
   echo_info 'maven 3.6 installing end'
 }
